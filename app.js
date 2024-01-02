14c3e1211b0f2e3d6161c7c3f290e9fec190e3bb
cheriiih <cherihogan123@gmail.com>
2024-01-02 12:41:23 -0500

    homepage

--- app.js
+++ app.js
@@ -7,21 +7,24 @@ let paintdrop1 = document.getElementById('paintdrop1');
 let paintdrop2 = document.getElementById('paintdrop2');
 let digitalartist = document.getElementById('digitalartist');
 
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', function() {
     let value = window.scrollY;
 
-    text1.style.marginTop = value * 2.5 + 'px';
-    text2.style.marginTop = value * 2.5 + 'px';
-    text3.style.marginTop = value * 2.5 + 'px';
-    btn.style.marginTop = value * 2.5 + 'px';
-    paintdrop1.style.left = value * -1.5 + 'px';
-    paintdrop2.style.left = value * 1.5 + 'px';
-   
+    // Set maximum and minimum values for marginTop
+    let marginTopValue = Math.min(Math.max(value * 2.5, 0), 500);
+    text1.style.marginTop = marginTopValue + 'px';
+    text2.style.marginTop = marginTopValue + 'px';
+    text3.style.marginTop = marginTopValue + 'px';
+    btn.style.marginTop = marginTopValue + 'px';
+
+    // Set maximum and minimum values for left
+    paintdrop1.style.left = Math.min(Math.max(value * -1.5, -100), 100) + 'px';
+    paintdrop2.style.left = Math.min(Math.max(value * 1.5, -100), 100) + 'px';
+
     text1.style.zIndex = -2;
     text2.style.zIndex = -2;
     text3.style.zIndex = -2;
     btn.style.zIndex = -1;
-    
-  
-   
 });
+
+
